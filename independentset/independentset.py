78e42724aa431d7c2b2608c831b996e64b061e27
Gustav Skytting <gustav.p.skytting@gmail.com>
2026-04-21 13:50:49 +0200

    refactor: update independent set algorithms with improved reduction rules and add benchmark logging

--- independentset/independentset.py
+++ independentset/independentset.py
@@ -1,3 +1,4 @@
+import math
 
 def readAdjacencyMatrix(filename):
     V = []
@@ -20,7 +21,13 @@ def readAdjacencyMatrix(filename):
             idx += 1   
     return (V,E)
     
+R0_count = 0
+R1_count = 0
+R2_count = 0
+
 def independentSetR0(graph, memo=None):
+    global R0_count
+    R0_count += 1
     if memo is None:
         memo = {}
         
@@ -32,17 +39,26 @@ def independentSetR0(graph, memo=None):
     if V in memo:
         return memo[V]
         
-    v_max = max(V, key=lambda v: len(E[v]))
+    for v in V:
+        active_neighbors = set(E[v]) & V
+        if len(active_neighbors) == 0:
+            res = 1 + independentSetR0((V - {v}, E), memo)
+            memo[V] = res
+            return res
+
+    u = max(V, key=lambda v: len(set(E[v]) & V))
     
-    res_exclude = independentSetR0((V - {v_max}, E), memo)
+    res_exclude = independentSetR0((V - {u}, E), memo)
     
-    neighbors = set(E[v_max]) & V
-    res_include = 1 + independentSetR0((V - {v_max} - neighbors, E), memo)
+    neighbors = set(E[u]) & V
+    res_include = 1 + independentSetR0((V - {u} - neighbors, E), memo)
     
     memo[V] = max(res_exclude, res_include)
     return memo[V]
 
 def independentSetR1(graph, memo=None):
+    global R1_count
+    R1_count += 1
     if memo is None:
         memo = {}
         
@@ -52,23 +68,28 @@ def independentSetR1(graph, memo=None):
         return 0
 
     for v in V:
-        if(len(set(E[v]) & V) == 1):
-            return 1 + independentSetR1((V - {v} - set(E[v]), E), memo)
+        active_neighbors = set(E[v]) & V
+        if len(active_neighbors) == 1:
+            return 1 + independentSetR1((V - {v} - active_neighbors, E), memo)
+        elif len(active_neighbors) == 0:
+            return 1 + independentSetR1((V - {v}, E), memo)
 
     if V in memo:
         return memo[V]
         
-    v_max = max(V, key=lambda v: len(E[v]))
+    u = max(V, key=lambda v: len(set(E[v]) & V))
     
-    res_exclude = independentSetR1((V - {v_max}, E), memo)
+    res_exclude = independentSetR1((V - {u}, E), memo)
     
-    neighbors = set(E[v_max]) & V
-    res_include = 1 + independentSetR1((V - {v_max} - neighbors, E), memo)
+    neighbors = set(E[u]) & V
+    res_include = 1 + independentSetR1((V - {u} - neighbors, E), memo)
     
     memo[V] = max(res_exclude, res_include)
     return memo[V]
 
 def independentSetR2(graph, memo=None):
+    global R2_count
+    R2_count += 1
     if memo is None:
         memo = {}
         
@@ -78,29 +99,58 @@ def independentSetR2(graph, memo=None):
         return 0
 
     for v in V:
-        if(len(set(E[v]) & V) == 2):
-            u, w = E[v][0], E[v][1]
-            if(u in E[w] and w in E[u]):
-                return 1 + independentSetR2((V - {v} - set(E[v]), E), memo)
+        active_neighbors = list(set(E[v]) & V)
+        degree = len(active_neighbors)
+        
+        if degree == 1:
+            return 1 + independentSetR2((V - {v} - set(active_neighbors), E), memo)
+        elif degree == 0:
+            return 1 + independentSetR2((V - {v}, E), memo)
+        elif degree == 2:
+            u, w = active_neighbors[0], active_neighbors[1]
+            if(u in E[w] or w in E[u]):
+                return 1 + independentSetR2((V - {v, u, w}, E), memo)
             else:
-                return 1 + independentSetR2((V - {v} - set(E[v]) | {-v}, E | {-v: E[u] + E[w]}), memo)
-        if(len(set(E[v]) & V) == 1):
-            return 1 + independentSetR2((V - {v} - set(E[v]), E), memo)
+                z = -v
+                z_neighbors = list((set(E[u]) | set(E[w])) - {v, u, w})
+                new_E = E.copy()
+                new_E[z] = z_neighbors
+                for neighbor in z_neighbors:
+                    new_E[neighbor] = list((set(E[neighbor]) - {u, w}) | {z})
+                return 1 + independentSetR2((V - {v, u, w} | {z}, new_E), memo)
 
     if V in memo:
         return memo[V]
         
-    v_max = max(V, key=lambda v: len(E[v]))
+    u = max(V, key=lambda v: len(set(E[v]) & V))
     
-    res_exclude = independentSetR2((V - {v_max}, E), memo)
+    res_exclude = independentSetR2((V - {u}, E), memo)
     
-    neighbors = set(E[v_max]) & V
-    res_include = 1 + independentSetR2((V - {v_max} - neighbors, E), memo)
+    neighbors = set(E[u]) & V
+    res_include = 1 + independentSetR2((V - {u} - neighbors, E), memo)
     
     memo[V] = max(res_exclude, res_include)
     return memo[V]
-    
-print(independentSetR2(readAdjacencyMatrix("data/g50.in")))
+
+for filename in ["data/g30.in", "data/g40.in", "data/g50.in", "data/g60.in"]:
+    R0_count = 0
+    res = independentSetR0(readAdjacencyMatrix(filename))
+    print(f"{filename}: MIS={res}, Nodes visited={round(math.log(R0_count), 1)}")
+
+print("---")
+
+for filename in ["data/g30.in", "data/g40.in", "data/g50.in", "data/g60.in", "data/g70.in", "data/g80.in", "data/g90.in", "data/g100.in"]:
+    R1_count = 0
+    res = independentSetR1(readAdjacencyMatrix(filename))
+    print(f"{filename}: MIS={res}, Nodes visited={round(math.log(R1_count), 1)}")
+
+print("---")
+
+for filename in ["data/g30.in", "data/g40.in", "data/g50.in", "data/g60.in", "data/g70.in", "data/g80.in", "data/g90.in", "data/g100.in", "data/g110.in", "data/g120.in"]:
+    R2_count = 0
+    res = independentSetR2(readAdjacencyMatrix(filename))
+    print(f"{filename}: MIS={res}, Nodes visited={round(math.log(R2_count), 1)}")
+
 
             
 
